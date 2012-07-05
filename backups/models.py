411df5211ea16d1c3de31f36726870458c86cd78
Guilherme Elias <guilherme.elias@gmail.com>
2012-07-05 01:24:59 -0300

    Corrigindo testes

--- backups/models.py
+++ backups/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
+import os
+
 class Backup:
 
-	def __init__(self, path,file_pattern):
+	def setUp(self):
+		file = None
+		try:
+			file = open('/temp/bckp.zip', 'w')
+		except Exception, e:
+			print 'Fudeu'
+		finally:
+			print file.name
+			file.close
+		
+
+	def __init__(self, path, file_pattern):
 	  self.path = path
 	  self.file_pattern = file_pattern
 
+	def how_is_it(self):
+		filepath = '%s/%s' % (self.path,self.file_pattern)
+		print filepath
+		print os.path.isfile(filepath)
+		return os.path.isfile(filepath)
--- backups/tests.py
+++ backups/tests.py
@@ -1,17 +1,26 @@
 from django.test import TestCase
 from backups.models import Backup
+import os
 
-class BackupTest(TestCase):
+class TestBackup(TestCase):
+    
+    def setUp(self):
+        print "setup ..."
+        try:
+            f = open('/temp/bckp.zip', 'w')
+            f.close
+        except Exception, e:
+            raise e
 
     def test_should_create_backup_file_instance(self):
-    	path = '/temp'
-		file_name = 'bckp*.zip'
-		self.backup = Backup(path, file_name)
-        self.assertIsNotNone(self.backup)
+        path = "/temp"
+        file_pattern = ""
+        backup = Backup(path,file_pattern)
+        self.assertIsNotNone(backup)
 
     def test_should_check_file_existing(self):
-    	path = '/temp'
-		file_name = 'bckp*.zip'
-		self.backup = Backup(path, file_name)
-        self.assertTrue(self.backup.itsHere)
+        path = "/temp"
+        file_pattern = "bckp.zip"
+        backup = Backup(path,file_pattern)
+        self.assertTrue(backup.how_is_it())
 
